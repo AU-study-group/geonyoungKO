2b0179fd2b8e62be06e81d73d5096ab3f971399e
geonyoungKO <kws07042@gmail.com>
2023-05-13 13:59:39 +0900

    ddd

--- Layout v1.2/app.js
+++ Layout v1.2/app.js
@@ -1,3 +1,3 @@
 import {menu} from "./main.js";//menu로 선언한 함수를 수입
 const web = document.querySelector('.web');// const web 선언 id가 web 요소를 가져옴
-web.insertAdjacentHTML('beforeend', menu);//web아래에 time1에 정의된 코드를 제일 끝에 붙인다.
+web.insertAdjacentHTML('beforeend', menu);//web아래에 menu에 정의된 코드를 제일 끝에 붙인다.
--- Layout v1.2/main.js
+++ Layout v1.2/main.js
@@ -1,6 +1,5 @@
 export const menu = `
 <h1 class = "제목">main</h1>
-<nav class = "navbar" >
     <a class = "asibal" href="#"><i class="fa-solid fa-bars"></i></a>
     <ul class = "nav">
         <li class = "il"><a href="#">menu1</a></li>
@@ -10,8 +9,18 @@ export const menu = `
         <li class = "il"><a href="#">menu5</a></li>
         <li class = "il"><a href="#">menu6</a></li>
         </ul>
-        </nav>  
-        <footer class = "asibal1">푸터</footer>
+  
+<footer class = "푸터">
+
+    <ul class = "푸터nav">
+      <li class = "il2"><a href="#">Home</a></li>
+      <li class = "il2"><a href="#">About</a></li>
+      <li class = "il2"><a href="#">Services</a></li>
+      <li class = "il2"><a href="#">Contact</a></li>
+    </ul>
+
+</footer>
+
 `;
 
 // 선언된  menu를 수출
