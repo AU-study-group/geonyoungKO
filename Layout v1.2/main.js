57b8a404f2994a8c5e4bfd604783512d0164f498
geonyoungKO <kws07042@gmail.com>
2023-05-14 20:29:08 +0900

    긑

--- Layout v1.2/main.js
+++ Layout v1.2/main.js
@@ -1,25 +1,26 @@
 export const menu = `
-<h1 class = "제목">main</h1>
-    <a class = "asibal" href="#"><i class="fa-solid fa-bars"></i></a>
-    <ul class = "nav">
-        <li class = "il"><a href="#">menu1</a></li>
-        <li class = "il"><a href="#">menu2</a></li>
-        <li class = "il"><a href="#">menu3</a></li>
-        <li class = "il"><a href="#">menu4</a></li>
-        <li class = "il"><a href="#">menu5</a></li>
-        <li class = "il"><a href="#">menu6</a></li>
-        </ul>
-  
-<footer class = "푸터">
-    <ul class = "푸터nav">
-      <li class = "il2"><a href="#">Home</a></li>
-      <li class = "il2"><a href="#">About</a></li>
-      <li class = "il2"><a href="#">Services</a></li>
-      <li class = "il2"><a href="#">Contact</a></li>
-    </ul>
 
+<h1 class="제목">HOME</h1>
+<a class="asibal" href="#"><i class="fa-solid fa-bars"></i></a>
+<ul class="nav">
+  <li class="il"><a href="#">menu1</a></li>
+  <li class="il"><a href="#">menu2</a></li>
+  <li class="il"><a href="#">menu3</a></li>
+  <li class="il"><a href="#">menu4</a></li>
+  <li class="il"><a href="#">menu5</a></li>
+  <li class="il"><a href="#">menu6</a></li>
+</ul>
+<footer class="푸터">
+  <ul class="푸터nav">
+    <li><a class="il2" href="#"><i class="푸터menu">Home</i></a></li>
+    <li><a class="il2" href="#"><i class="푸터menu">About</i></a></li>
+    <li><a class="il2" href="#"><i class="푸터menu">Services</i></a></li>
+    <li><a class="il2" href="#"><i class="푸터menu">Contact</i></a></li>
+    <li><a class="il2" href="#"><i class="fa brands fa-youtube"></i></a></li>
+    <li><a class="il2" href="#"><i class="fa brands fa-twitter"></i></a></li>
+    <li><a class="il2" href="#"><i class="fa brands fa-instagram"></i></a></li>
+  </ul>
 </footer>
-
 `;
 
 // 선언된  menu를 수출
